ef0f0fbc33d647c3dcfdf1d8cebba133b0dc52aa
etileyli <etileyli@hotmail.com>
2020-03-23 20:35:08 +0300

    methods performed.

--- gettingStarted/chp3_ex1.py
+++ gettingStarted/chp3_ex1.py
@@ -1,4 +1,24 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+message = "My first bicycle was a " + bicycles[3].title() + "."
+print(message)
+bicycles.append('sth')
+message = "My first bicycle was a " + bicycles[4].title() + "."
+print(message)
+bicycles.insert(0, 'new bicycle')
 print(bicycles)
+del bicycles[0]
+print(bicycles)
+print('')
+bicycles.sort()
+print(bicycles)
+bicycles.sort(reverse=True)
+print(bicycles)
+print(sorted(bicycles))
 
-print(bicycles[0])
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+bicycles.reverse()
+print(bicycles)
+print(bicycles[-1])
+
+for bicycle in bicycles:
+    print(bicycle)
