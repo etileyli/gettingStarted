59108be1e686e63a0014f0e1455412ce98e5d7ef
etileyli <etileyli@hotmail.com>
2020-03-29 16:11:00 +0300

    simple examples

---
+++ gettingStarted/chp5_ex1.py
@@ -0,0 +1,19 @@
+cars = ['audi', 'renault', 'mercedes', 'bmw']
+
+for car in cars:
+    if (car == 'bmw'):
+        print(car)
+    else:
+        print('not a bmw')
+
+cars[1].upper()
+print(cars[1])
+
+print('audi' not in cars)
+
+print("Is car == 'subaru'? I predict True.")
+
+if cars:
+    print('cars is a list')
+else:
+    print('cars list is empty')
