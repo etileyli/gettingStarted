992d80ca80a173d6b9b8f312fb4759dac6f60eed
etileyli <etileyli@hotmail.com>
2020-03-29 17:37:22 +0300

    basic examples

--- gettingStarted/chp6_ex2.py
+++ gettingStarted/chp6_ex2.py
@@ -4,5 +4,18 @@ favorite_languages = {
     'gizem': 'csharp',
 }
 
-print(favorite_languages)
+# print(favorite_languages)
 
+# for key, value in favorite_languages.items():
+#     print('\nPerson: ' + key)
+#     print('Favorite language: ' + value)
+
+i = 0
+for key in favorite_languages:
+    i = i + 1
+    print('\nKey ' + str(i) + ': ' + key)
+
+i = 0
+for key in sorted(favorite_languages):
+    i = i + 1
+    print('\nKey ' + str(i) + ': ' + key)
