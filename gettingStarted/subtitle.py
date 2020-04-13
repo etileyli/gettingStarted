14125a5792b7bd796f4023b19f7a0e7372164dec
etileyli <etileyli@hotmail.com>
2020-04-13 21:08:24 +0300

    subtitles

---
+++ gettingStarted/subtitle.py
@@ -0,0 +1,16 @@
+import os
+
+# for value in range(1, 10):
+#     src = "D:\Torrent\Friends Complete 1080p Bluray AAC 5.1 x265 HEVC-LION[UTR]\Friends Season 02 S02 1080p Bluray AAC 5.1 x265 HEVC-LION[UTR]\\" + "friends.s02e0" + str(value) + ".720p.bluray.x264-psychd.srt"
+#     dst = "D:\Torrent\Friends Complete 1080p Bluray AAC 5.1 x265 HEVC-LION[UTR]\Friends Season 02 S02 1080p Bluray AAC 5.1 x265 HEVC-LION[UTR]\\" + "Friends.S02E0" + str(value) + ".1080p.Bluray.5.1.x265-LION[UTR].srt"
+#     os.rename(src, dst)
+
+#for value in range(16, 25):
+#    src = "D:\Torrent\Friends Complete 1080p Bluray AAC 5.1 x265 HEVC-LION[UTR]\Friends Season 02 S02 1080p Bluray AAC 5.1 x265 HEVC-LION[UTR]\\" + "friends.s02e" + str(value) + ".720p.bluray.x264-intention.srt"
+#    dst = "D:\Torrent\Friends Complete 1080p Bluray AAC 5.1 x265 HEVC-LION[UTR]\Friends Season 02 S02 1080p Bluray AAC 5.1 x265 HEVC-LION[UTR]\\" + "Friends.S02E" + str(value) + ".1080p.Bluray.5.1.x265-LION[UTR].srt"
+#    os.rename(src, dst)
+
+for val in range(1, 10):
+	print("with a " + str(val))
+
+input(v)
