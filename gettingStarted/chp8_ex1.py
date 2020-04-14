068ea678cf313c52e82982613ce38e908b6a701a
etileyli <etileyli@hotmail.com>
2020-04-14 15:42:14 +0300

    minor updates

--- gettingStarted/chp8_ex1.py
+++ gettingStarted/chp8_ex1.py
@@ -6,13 +6,18 @@ def greet_user():
 greet_user()
 
 
-def describe_pet(animal_type= 'cat', pet_name= 'misiklet'):
+# def describe_pet(animal_type= 'cat', pet_name= 'misiklet'):
+    # """Display information about pet."""
+    # print("\nI have a " + animal_type + ".")
+    # print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+def describe_pet(animal_type, pet_name= 'misiklet'):
     """Display information about pet."""
     print("\nI have a " + animal_type + ".")
     print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 
 
 describe_pet(animal_type='kedi', pet_name='miskos')
-describe_pet(pet_name='miskos')
+# describe_pet(pet_name='miskos')
 describe_pet(animal_type='cat')
 
