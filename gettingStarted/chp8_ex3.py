4b4f8626998bd23880cfbe2019569af40d9f4cc3
etileyli <etileyli@hotmail.com>
2020-04-14 16:11:46 +0300

    minor updates

---
+++ gettingStarted/chp8_ex3.py
@@ -0,0 +1,21 @@
+def make_pizza(size, *toppings):
+    """Summarize the pizza we are about to make."""
+    print("\nMaking a " + str(size) +
+          "-inch pizza with the following toppings:")
+    for topping in toppings:
+        print("- " + topping)
+
+make_pizza(16, 'pepperoni')
+make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+
+def user_profile(first, last, **user_info):
+    """Build a dictionary that contains everything we know about the user."""
+    profile = {}
+    profile['first_name'] = first
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+
+prof = user_profile ('albert', 'einstein', location= 'Princeton', major='physics')
+print(prof)
