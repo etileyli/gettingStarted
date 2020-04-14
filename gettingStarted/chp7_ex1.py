78b0e8a36b5d7fa798975a329658bfaab4c96ac9
etileyli <etileyli@hotmail.com>
2020-04-14 08:32:37 +0300

    basic examples

---
+++ gettingStarted/chp7_ex1.py
@@ -0,0 +1,12 @@
+# message = input("Tell me something and I repeat it:\n")
+# print(message)
+
+currentNumber = 0
+while currentNumber <= 5:
+    print(currentNumber)
+    currentNumber += 1
+
+message = ""
+while message != "quit":
+    message = input("enter message:\n")
+    print(message)
