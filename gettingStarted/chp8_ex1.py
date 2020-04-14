b16e16a0152c0f1605d0ea2ed6081e04ccc47557
etileyli <etileyli@hotmail.com>
2020-04-14 15:33:54 +0300

    minor updates

---
+++ gettingStarted/chp8_ex1.py
@@ -0,0 +1,18 @@
+def greet_user():
+    """Display a simple greeting!"""
+    print("Hello!")
+
+
+greet_user()
+
+
+def describe_pet(animal_type= 'cat', pet_name= 'misiklet'):
+    """Display information about pet."""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+describe_pet(animal_type='kedi', pet_name='miskos')
+describe_pet(pet_name='miskos')
+describe_pet(animal_type='cat')
+
