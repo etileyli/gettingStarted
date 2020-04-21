9d17120484c33e25785d793d46acff61633d1ba5
etileyli <etileyli@hotmail.com>
2020-04-21 18:17:42 +0300

    few updates. works correctly as in Figure 18-4: The topics page

--- gettingStarted/learning_log/learning_logs/urls.py
+++ gettingStarted/learning_log/learning_logs/urls.py
@@ -2,7 +2,13 @@
 from django.conf.urls import url
 from . import views
 
+
+app_name = 'temp'
+
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
+
+    # Show all topics.
+    url(r'^topics/$', views.topics, name='topics'),
 ]
