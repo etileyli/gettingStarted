58aec86b4e09a00aeb4e99627ca3b87b1db9132d
etileyli <etileyli@hotmail.com>
2022-10-29 17:33:56 +0300

    Revert "minor update"
    This reverts commit ec21ed2e

--- gettingStarted/helloWorld.py
+++ gettingStarted/helloWorld.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     print("I'm a car!")
     while True:
         action = input("What should I do? [A]ccelerate, [B]rake, " "show [O]dometer, or show average [S]peed?").upper()
-        if action not in "ABOSQ" or len(action) != 1:
+        if action not in "ABOS" or len(action) != 1:
             print("I don't know how to do that")
             continue
         if action == 'A':
@@ -41,9 +41,8 @@ if __name__ == '__main__':
             print("The car has driven {} kilometers".format(my_car.odometer))
         elif action == 'S':
             print("The car's average speed was {} kph".format(my_car.average_speed()))
-        elif action == 'Q':
+        elif action == 'q':
             print("Quitting!!")
-            exit(0)
 
         my_car.step()
         my_car.say_state()
